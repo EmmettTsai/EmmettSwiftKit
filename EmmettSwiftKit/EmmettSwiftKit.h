b225715d3ce04a001f09715828a0c35b3c7ffff7
Emmett Tsai <emmett.tsai@gmail.com>
2019-01-19 00:10:14 +0800

    Initial Commit

---
+++ EmmettSwiftKit/EmmettSwiftKit.h
@@ -0,0 +1,16 @@
+//
+//  Created by Emmett Tsai on 2019/1/19.
+//  Copyright © 2019 Emmett Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmmettSwiftKit.
+FOUNDATION_EXPORT double EmmettSwiftKitVersionNumber;
+
+//! Project version string for EmmettSwiftKit.
+FOUNDATION_EXPORT const unsigned char EmmettSwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmmettSwiftKit/PublicHeader.h>
+
+
